617029b1571dddf81082a949377982819c0d45e6
SUMMER\quanh <quanhavo005@gmail.com>
2024-07-18 23:40:25 +0700

    add config path for ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 
-import reactLogo from './assets/react.svg';
+import { default as reactLogo, default as viteLogo } from '@/assets/react.svg';
 
-import './App.css';
-
-import viteLogo from '/vite.svg';
+import '@/App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [, setCount] = useState(0);
 
   return (
     <>
@@ -24,9 +22,7 @@ function App() {
         <button
           onClick={() => {
             setCount(count => count + 1);
-          }}>
-          count is {count}
-        </button>
+          }}></button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
